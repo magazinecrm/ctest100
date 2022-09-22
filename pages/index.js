ee9ae1d42ec161c8c75d9ff6227749dc7dab1c77
magazinecrm <paul@magazinecrm.com>
2022-09-22 19:54:49 +0300

    changed commit

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcomsfslfssldfkje to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get startsssed by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
